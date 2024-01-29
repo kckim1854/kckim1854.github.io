8fb8351c4c52e490f4e36893ef65822c33b95ccd
tigrewuz <tigrewuz@naver.com>
2024-01-29 13:45:50 +0900

    css 브랜치 사고로 사라짐

--- djweb/settings.py
+++ djweb/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mzvcsport_i%qyc#vb*tooj8#+tiqiymv(76r4or*-0mp73y9-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
